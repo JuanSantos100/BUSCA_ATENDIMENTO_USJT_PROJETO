2322c0f99f9615c5fd1410445a613108c1be4983
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-10-04 22:13:10 -0300

    Trabalhando no serviço de login. Em desenvolvimento

---
+++ login/login.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended : false}));
+
+app.get("/",function(req,res){
+	res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/logar", (req,res) => {
+    let usuario = req.body.usuario;
+    let senha = req.body.senha;
+    let usuarios = [["Caetano","ocaets","cliente"], 
+    ["Juan","ojuju","master"], 
+    ["Farfan", "ofarfas","hospital"]]
+    
+    for (let x=0; x < usuarios.length; x++){
+        if (usuarios[x][0] == usuario){
+            if(usuarios[x][1] == senha){
+                if (usuarios[x][2] == 'cliente') {
+                    console.log('cheguei aqui...')
+                    res.status(200).send("Seja bem vindo cliente")
+                }
+                else if (usuarios[x][2] == 'hospital') {
+                    res.status(200).send("Seja bem vindo hospital")
+                    }
+                else if (usuarios[x][2] == 'master') {
+                    res.status(200).send("Seja bem vindo master")
+                }
+                else {
+                    res.status(401).send("Usuário não classificado")
+                }
+            }            
+                    
+        
+        else{
+            res.end("Senha Incorreta!")
+        }
+    }	
+    else {
+res.end("Usuario não existe!");
+    }
+        
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Login, porta 3000")
+});
