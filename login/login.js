01b2f97e19639bcc3729ffa8bf2901e622d11212
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-10-07 23:32:57 -0300

    Finalizei o serviço de login, está pronto para uso.

--- login/login.js
+++ login/login.js
@@ -5,46 +5,40 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended : false}));
 
 app.get("/",function(req,res){
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/login.html");
 })
 
 app.post("/logar", (req,res) => {
     let usuario = req.body.usuario;
     let senha = req.body.senha;
-    let usuarios = [["Caetano","ocaets","cliente"], 
-    ["Juan","ojuju","master"], 
-    ["Farfan", "ofarfas","hospital"]]
+    // let tipo_usuario = req.body.tipo_usuario;
+    console.log("Usuário: " + usuario + ' Senha: ' + senha)
     
-    for (let x=0; x < usuarios.length; x++){
-        if (usuarios[x][0] == usuario){
-            if(usuarios[x][1] == senha){
-                if (usuarios[x][2] == 'cliente') {
-                    console.log('cheguei aqui...')
-                    res.status(200).send("Seja bem vindo cliente")
-                }
-                else if (usuarios[x][2] == 'hospital') {
-                    res.status(200).send("Seja bem vindo hospital")
-                    }
-                else if (usuarios[x][2] == 'master') {
-                    res.status(200).send("Seja bem vindo master")
-                }
-                else {
-                    res.status(401).send("Usuário não classificado")
-                }
-            }            
-                    
-        
-        else{
-            res.end("Senha Incorreta!")
-        }
-    }	
-    else {
-res.end("Usuario não existe!");
-    }
-        
-    }
-})
+    let base_usuarios = [["Juan", "123", 1], ["Caetano", "456", 2], ["Farfan", "789", 3]]
+    // Funcionando
 
-app.listen(3000, () => {
-    console.log("Login, porta 3000")
-});
+    for(let x = 0; x < base_usuarios.length; x++) {
+        if (usuario == base_usuarios[x][0]) { //Verificação de usuário
+            if(senha == base_usuarios[x][1]) { //Verificação da senha
+                if (base_usuarios[x][2] == 1) { //Diferenciação de login
+                    res.status(200).send("Seja bem vindo " + usuario + "!! Você é admin master! ") 
+                } else if (base_usuarios[x][2] == 2) {
+                    res.status(200).send("Seja bem vindo " + usuario + "!! Você é Hospital! ")
+                } else if (base_usuarios[x][2] == 3) {
+                    res.status(200).send("Seja bem vindo " + usuario + "!! Você é paciente! ")
+                }      
+            } else {
+                res.status(401).send("Usuário ou senha incorreta")
+            }
+        } 
+    }
+    res.status(404).send("Usuário não existe")
+
+    
+
+})
+   
+app.listen(3000, () => {console.log("Serviço de login. porta 3000")})
+    
+
+
