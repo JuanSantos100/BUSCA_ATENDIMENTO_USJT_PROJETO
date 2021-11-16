7018be26e8a8e0ad054b6d18068884a2c292f8c4
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-11-15 23:13:55 -0300

    Operações de hospitais realizadas com banco de dados

--- projeto-main/hospital/operacoes/operacoes_hospital.js
+++ projeto-main/hospital/operacoes/operacoes_hospital.js
@@ -1,68 +1,123 @@
+require('dotenv').config()
+const mysql = require('mysql2')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-let id_hospital = 1
-let hospitais = []
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE} = process.env
+
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10
+})
 
 
 //Cadastro de hosptital
-app.post('/hospital/cadastro', (req,res) => {
-    console.log(req.body)
-    
-    const hospital = {
-        id_hospital: id_hospital++,
-        nome_hospital: req.body.nome_hospital,
-        tipo_hospital: req.body.tipo_hospital,
-        capacidade_atendimento : req.body.capacidade_atendimento,
-        qtd_leitos_disponiveis : req.body.qtd_leitos_disponiveis,
-        telefone: req.body.telefone,
-        horario_funcionamento: req.body.horario_funcionamento
-    }
-    hospitais.push(hospital)
-    res.status(201).json(hospital)
+app.post('/hospital/cadastro', (req, res) => {
+    const nome_hospital = req.body.nome_hospital
+    const cep = req.body.cep
+    const endereco_hospital = req.body.endereco_hospital
+    const qtd_leitos = req.body.qtd_leitos_disponiveis
+    const capacidade_atendimento = req.body.capacidade_atendimento
+
+    const sql = `
+        INSERT INTO HOSPITAL (NM_HOSPITAL, CEP, ENDERECO_HOSPITAL, QTD_LEITOS, CPD_ATENDIMENTO) VALUES (?, ?, ?, ? , ?)
+    `
+    pool.query(
+        sql,
+        [nome_hospital, cep, endereco_hospital, qtd_leitos, capacidade_atendimento],
+        (err, resuls, fields) => {
+            console.log(resuls)
+            res.send('Operação realizada com sucesso !')
+        }
+    )
 })
 
-//Deletar um hospital
+//DELETE DE HOSPITAIS
 app.delete('/hospital/delete/:id', (req, res) => {
-    const hospital_id = +req.params.id
-    console.log(hospital_id)
+    const cd_hospital = +req.params.id
 
-    const deleted = hospitais.find(hospital => hospital.id_hospital === hospital_id) //Encontrar o id do hospital
-    // console.log(deleted)
-    if (deleted) {
-       hospitais = hospitais.filter(hospital => hospital.id_hospital !== hospital_id) 
-       res.status(200).json(deleted)
-    } else {
-        
-        res.status(404).json({mensagem: "Hospital não encontrado"})
-    }
+    // const sql_consulta = `
+    //     SELECT * FROM HOSPITAL WHERE CD_HOSPITAL = ?
+    // `
+    const sql = `
+        DELETE FROM HOSPITAL
+        WHERE CD_HOSPITAL = ?
+    
+    `
+
+    // NECESSÁRIO IMPLEMENTAR CASO NÃO ENCONTRE O HOSPITAL NO BANCO DE DADOS
+
+    pool.query(
+        sql,
+        [cd_hospital],
+        (err, results, fields) => {
+            console.log(results)
+            res.send('Operação realizada com sucesso !')
+        }
+    )
 })
 
-//Consulta de hospital
+//CONSULTA DE 1 HOSPITAL
+app.get('/hospital/consulta/:id', (req, res) => {
+    const cd_hospital = +req.params.id
+    const sql = `SELECT * FROM HOSPITAL WHERE CD_HOSPITAL = ?`
+
+    pool.query(
+        sql,
+        [cd_hospital],
+        (err, results, fields) => {
+            console.log(results)
+            res.json(results)
+        }
+
+    )
+})
+
+//CONSULTA DE TODOS OS HOSPITAIS
 app.get('/hospital/consulta', (req, res) => {
-    res.json(hospitais)
+    const sql = `SELECT * FROM HOSPITAL`
+
+    pool.query(
+        sql,
+        (err, results, fields) => {
+            console.log(results)
+            res.json(results)
+        }
+
+    )
 })
 
 //Atualização de dados do hospital
 app.put('/hospital/atualizacao/:id', (req, res) => {
-    const hospital_id = +req.params.id
-    console.log(hospital_id)
-    const hospital_atualizado = hospitais.find(hospital => hospital.id_hospital === hospital_id)
-    console.log(hospital_atualizado)
+    const cd_hospital = req.params.id
+    const qtd_leitos = req.body.qtd_leitos
+    const capacidade_atendimento = req.body.capacidade_atendimento
 
+    const sql = `
+        UPDATE HOSPITAL
+        SET QTD_LEITOS = ?,
+        CPD_ATENDIMENTO = ?
+        WHERE CD_HOSPITAL = ?
+    
+    `
 
-    if (hospital_atualizado) {
-        hospital_atualizado.capacidade_atendimento = req.body.capacidade_atendimento
-        hospital_atualizado.qtd_leitos_disponiveis = req.body.qtd_leitos_disponiveis
-        res.status(200).json(hospital_atualizado)
-    } else {
-        res.status(404).json({mensagem: "Hospital não encontrado"})
-    }
+    pool.query(
+        sql,
+        [qtd_leitos, capacidade_atendimento, cd_hospital],
+        (err, results, fields) => {
+            console.log(results)
+            res.send('Operação realizada com sucesso !')        
+        }
+
+    )
 })
-
 
 
 app.listen(3001, () => {
