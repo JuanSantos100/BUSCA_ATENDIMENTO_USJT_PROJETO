6617620e086efa42f026258d454d13ff3b0a0bcc
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-11-17 23:39:35 -0300

    Necessário ajustar o método de login | Problemas no processamento de informações que vem do formulário

--- projeto-main/login/login.js
+++ projeto-main/login/login.js
@@ -1,16 +1,48 @@
 const express = require('express');
+require('dotenv').config()
+const mysql =  require('mysql2')
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { response } = require('express');
+const { emit } = require('nodemon');
 const app = express();
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'master',
+//     database: 'busca_atendimento'
+// })
+
+// db.connect((error) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log("MYSQL Connected ! ")
+//     }
+// })
+
+//Criando pool de conexões
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10
+})
+
 app.get('/', (req, res) => {
     // res.send('Meu servidor backend já está online !')
     return res.json(base_usuarios)
 })
+
+
 
 let base_usuarios = [
     {
@@ -22,6 +54,44 @@ let base_usuarios = [
         password: "456"
     }
 ]
+
+app.get('/login', (req, res) => {
+    
+    const email = req.body.email
+    // console.log(email)
+    const senha = req.body.senha
+    const sql = 
+    `
+        SELECT EMAIL, SENHA FROM PACIENTE WHERE EMAIL = ?
+    `
+    pool.query(
+        sql,
+        [email],
+        (errors, results, fields) => {
+            if (errors) {
+                console.log(errors)
+            }
+
+            /*
+            Pegando valores do results
+
+            // console.log(results[0]['EMAIL'])
+            // console.log(results[0]['SENHA'])
+            */
+
+            //Erro nesta seção de código !! Mensagem: Cannot read property 'EMAIL' of undefined
+            //Observar a tela \PROJETO\front\screens\Login.js 
+            if(results[0]['EMAIL'] == email && results[0]['SENHA'] == senha) {
+                console.log('Logado!')
+                return res.json(results)
+            } else {
+                console.log('Tentativa de login falhou ! Usuário ou senha inválidos')
+                return res.send(JSON.stringify('Tentativa de login falhou ! Usuário ou senha inválidos'))
+            } 
+        
+        }
+    )
+})
 
 
 // app.post('/login', (req, res) => {
@@ -42,17 +112,17 @@ let base_usuarios = [
 //     }
 // })
 
-app.post('/login', (req, res) => {
-    console.log(req.body)
+// app.post('/login', (req, res) => {
+//     console.log(req.body)
 
-    if (base_usuarios.find(usr => usr.email == req.body.email && usr.password == req.body.password)) {
-        res.send(response)
-        console.log(response)
-    } else {
-        res.send(JSON.stringify('ERROR'))
-        // res.status(404).send("Usuário ou Senha inválidos")
-    }
-})
+//     if (base_usuarios.find(usr => usr.email == req.body.email && usr.password == req.body.password)) {
+//         res.send(response)
+//         console.log(response)
+//     } else {
+//         res.send(JSON.stringify('ERROR'))
+//         // res.status(404).send("Usuário ou Senha inválidos")
+//     }
+// })
 
 let porta = process.env.PORT || 3000;
 
